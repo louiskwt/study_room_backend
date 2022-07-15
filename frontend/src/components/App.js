42ee72b119961b9e302437fb936c4e44b66d4cc6
Louis Tsang <kawingt2@gmail.com>
2022-07-15 09:56:58 +0800

    add functional component

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,8 +1,13 @@
-const App = () => {
-  return <div>Hello Django and React</div>;
-};
+import React from "react";
+import { render } from "react-dom";
 
-export default App;
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, React and Django!!</h1>
+    </div>
+  );
+}
 
 const appDiv = document.getElementById("app");
 render(<App />, appDiv);
