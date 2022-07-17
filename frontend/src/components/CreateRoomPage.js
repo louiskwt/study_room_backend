9a7ebfe3e6a4993b2e2c5612e1757d35adeca776
Louis Tsang <kawingt2@gmail.com>
2022-07-18 07:19:29 +0800

    set up create room page

--- frontend/src/components/CreateRoomPage.js
+++ frontend/src/components/CreateRoomPage.js
@@ -1,7 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import { Link } from "react-router-dom";
 
 const CreateRoomPage = () => {
-  return <h1>CreateRoomPage</h1>;
+  const [vote, setVote] = useState(2);
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12} align="center">
+        <Typography variant="h4">Create a Room</Typography>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldset">
+          <FormHelperText>
+            <div align="center">Guest Control of Playback State</div>
+          </FormHelperText>
+          <RadioGroup row defaultValue="true">
+            <FormControlLabel
+              value="true"
+              control={<Radio color="primary" />}
+              label="Play/Pause"
+              labelPlacement="bottom"
+            />
+            <FormControlLabel
+              value="false"
+              control={<Radio color="secondary" />}
+              label="No control"
+              labelPlacement="bottom"
+            />
+          </RadioGroup>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <FormControl component="fieldset">
+          <TextField
+            required={true}
+            type="number"
+            defaultValue={vote}
+            inputProps={{ min: 1, style: { textAlign: "center" } }}
+          />
+          <FormHelperText>
+            <div align="center">Votes required to Skip Songs</div>
+          </FormHelperText>
+        </FormControl>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button color="primary" variant="contained">
+          Create a Room
+        </Button>
+      </Grid>
+      <Grid item xs={12} align="center">
+        <Button color="secondary" variant="contained" to="/" component={Link}>
+          Back
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default CreateRoomPage;
--- frontend/src/components/RoomJoinPage.js
+++ frontend/src/components/RoomJoinPage.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
+// import Button from "@material-ui/core/Button";
+// import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+// import TextField from "@material-ui/core/TextField";
+// import FormHelperText from "@material-ui/core/FormHelperText";
+// import FormControl from "@material-ui/core/FormControl";
+// import Radio from "@material-ui/core/Radio";
+// import RadioGroup from "@material-ui/core/RadioGroup";
+// import FormControlLabel from "@material-ui/core/FormControlLabel";
+// import { Link } from "react-router-dom";
 
 const RoomJoinPage = () => {
-  return <h1>RoomJoinPage</h1>;
+  const [vote, setVote] = useState(2);
+  return <Typography variant="h3">Join a room</Typography>;
 };
 
 export default RoomJoinPage;
