29c8ad4c44e9ab35ce2a4d831224b62989bf115f
Louis Tsang <kawingt2@gmail.com>
2022-07-17 17:23:14 +0800

    set up post request to create room

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RoomView
+from .views import RoomView, CreateRoomView
 
 urlpatterns = [
     path('room', RoomView.as_view(), name='home'),
+    path('create-room', CreateRoomView.as_view(), name='create-room'),
 ]
